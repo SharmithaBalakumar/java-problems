45c3606a5808c7be82ffe8bb909943d1316b6b18
SharmithaBalakumar <sharmithaabalakumar@gmail.com>
2026-08-10 15:35:12 +0530

    Convert days into years

---
+++ Variables,DataTypes,Input & Output/p5.java
@@ -0,0 +1,11 @@
+//Convert days into years
+import java.util.Scanner;
+public class p5{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int days = sc.nextInt();
+        int years = days/365;
+        System.out.print(years);
+        
+    }
+}
